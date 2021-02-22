eca40f384d6986a8b290c14ea734a0eaa1711f72
Ezequiel Salagoity <ezesalagoity@gmail.com>
2021-02-22 01:31:01 -0300

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,18 @@
+
+const {io} = require('../index');
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+    //client.on('event', data => { /* … */ });
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado')
+    });
+
+    client.on('mensaje',(payload)=>{
+        console.log('Mensaje',payload);
+
+        io.emit('mensaje',{admin: 'Nuevo mensaje'})
+    });
+
+});
